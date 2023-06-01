020c3b5c5fd850aa169470e0ce06ca627b9d85a6
Antoine A <antoine_avenia@hotmail.fr>
2023-06-01 14:53:32 +0200

    add nav bar style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,21 @@
-const NavBar = () => {
-  return (
-    <div>
-        <a href="/">Home</a>
-        <a href="/search">Search</a>
-        <a href="/history">History</a>
+import { Link } from 'react-router-dom';
+import logo from '../../assets/react.svg';
+import './NavBar.css';
 
-    </div>
-  )
+function NavBar(){
+  return (
+      <div className="banniere">
+          <div>
+              <img src={logo} className="img_logo" alt="logo" />
+              <h1 className="titre">Bienvenue sur l'app météo</h1>
+          </div>
+          <div>
+              <Link to="/"><button className="btn">Home</button></Link>
+              <Link to="/search"><button className="btn">Recherche</button></Link>
+              <Link to="/history"><button className="btn">Historique</button></Link>
+          </div>
+      </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
